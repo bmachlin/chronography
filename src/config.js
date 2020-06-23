6e54ab37564f0ec05bfeec55cf8531b8b0bfb0d7
Ben <ben@machlin.net>
2020-06-22 19:42:16 -0600

    config stuff

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 let config = {
     client: "7fc1ac4a76fe46fbb0d8b2791512daf2",
     // secret: "9eb898b7b90c48c1a01cf298199bcf44",
-    // redirect: "https://chronography.herokuapp.com"
-    redirect: "http://localhost:5000/"
+    redirect: "https://chronography.herokuapp.com"
+    // redirect: "http://localhost:5000/"
 }
 // todo: move
